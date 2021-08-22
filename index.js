0228edffaea14eba466751b7e09b0cd1d2557f07
Holly Tunison <hollytunison@gmail.com>
2021-08-22 13:07:12 -0400

    need to fix add new movie, get single movie, and delete single movie.

--- index.js
+++ index.js
@@ -1,67 +1,144 @@
 const express = require('express'),
-    morgan = require('morgan');
+    morgan = require('morgan'),
+    bodyParser = require('body-parser'),
+    uuid = require('uuid');
 
 const app = express();
 
-
+// Logging middleware
 app.use(morgan('common'));
+// Sending static files
 app.use(express.static('public'));
+// Using body-parser
+app.use(bodyParser.json());
 
+// Array of objects with my top ten movies
 let topMovies = [{
-        movie: 'The Godfather',
-        director: 'Francis Ford Coppola'
+        Id: '1',
+        title: 'The Godfather',
+        director: 'Francis Ford Coppola',
+        year: '1972'
     },
     {
-        movie: 'Pulp Fiction',
-        director: 'Quentin Tarantino'
+        Id: '2',
+        title: 'Pulp Fiction',
+        director: 'Quentin Tarantino',
+        year: '1994'
     },
     {
-        movie: 'The Shawshank Redemption',
-        director: 'Frank Darabont'
+        Id: '3',
+        mtitle: 'The Shawshank Redemption',
+        director: 'Frank Darabont',
+        year: '1994'
     },
     {
-        movie: 'The Good, the Bad and the Ugly',
-        director: 'Sergio Leone'
+        Id: '4',
+        title: 'The Good, the Bad and the Ugly',
+        director: 'Sergio Leone',
+        year: '1966'
     },
     {
-        movie: 'Fight Club',
-        director: 'David Fincher'
+        Id: '5',
+        title: 'Fight Club',
+        director: 'David Fincher',
+        year: '1999'
     },
     {
-        movie: 'Gran Torino',
-        director: 'Clint Eastwood'
+        Id: '6',
+        title: 'Gran Torino',
+        director: 'Clint Eastwood',
+        year: '2008'
     },
     {
-        movie: 'Braveheart',
-        director: 'Mel Gibson'
+        Id: '7',
+        title: 'Braveheart',
+        director: 'Mel Gibson',
+        year: '1995'
     },
     {
-        movie: 'Star Wars',
-        director: 'George Lucas'
+        Id: '8',
+        title: 'Star Wars',
+        director: 'George Lucas',
+        year: '1977'
     },
     {
-        movie: 'Good Will Hunting',
-        director: 'Gus Van Sant'
+        Id: '9',
+        title: 'Good Will Hunting',
+        director: 'Gus Van Sant',
+        year: '1997'
     },
     {
-        movie: 'Apocalypse Now',
-        director: 'Francis Ford Coppola'
+        Id: '10',
+        title: 'Apocalypse Now',
+        director: 'Francis Ford Coppola',
+        year: '1979'
     }
 ];
 
-
-// GET requests
-app.get('/', (req, res) => {
-    res.send('Welcome to myFlix! The best movie database!');
+//GET requests of all movies
+app.get('/movies', (req, res) => {
+    res.json(topMovies);
 });
 
+// Return a welcome message
+app.get('/', (req, res) => {
+    res.send('Welcome to myFlix Database');
+});
+
+// GET data about a single movie by its title
+app.get('/movies/:title', (req, res) => {
+    res.json(topMovies.find((movie) => {
+        return movie.title === req.params.title
+    }));
+});
+
+// Adds a new movie
+app.post('/movies', (req, res) => {
+    let newMovie = req.body;
+
+    if (!newMovie.title) {
+        const message = 'Missing movie title in request body';
+        res.status(400).send(message);
+    } else {
+        newMovie.id = uuid.v4();
+        topMovies.push(newMovie);
+        res.status(201).send(newMovie);
+    }
+});
+
+// Delete a movie from the list by ID
+app.delete('/movies/:id', (req, res) => {
+    let movie = topMovies.find((movie) => {
+        return movie.id === req.params.id
+    });
+
+    if (movie) {
+        topMovies = topMovies.filter((obj) => { return obj.id !== req.params.id });
+        res.status(201).send('Movie with the ID of ' + req.params.id + ' was deleted.');
+    } else {
+        res.status(404).send(`Movie with the id ${req.params.id} was not found.`);
+    }
+});
+
+
+// Update the year of a movie by its director
+app.put('/movies/:title/:director', (req, res) => {
+    let movie = topMovies.find((movie) => {
+        return movie.title = req.params.title
+    });
+    if (movie) {
+        movie.director = parseInt(req.params.director);
+        res.status(201).send(`Movie ${req.params.title} was assigned a director of ${req.params.director}`);
+    } else {
+        res.status(404).send(`Movie wit the title ${req.params.title} was not found.`);
+    }
+});
+
+// Get data documentation page
 app.get('/documentation', (req, res) => {
     res.sendFile('public/documentation.html', { root: __dirname });
 });
 
-app.get('/movies', (req, res) => {
-    res.json(topMovies);
-});
 
 // Error messages
 app.use((err, req, res, next) => {
@@ -70,6 +147,6 @@ app.use((err, req, res, next) => {
 });
 
 // listen for requests
-app.listen(8080, () => {
-    console.log('Your app is listening on port 8080.');
+app.listen(5500, () => {
+    console.log('Your app is listening on port 5500.');
 });
