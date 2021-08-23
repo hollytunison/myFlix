97ef86f96a0c23c093c4b1fed7ed54302a65d177
Holly Tunison <hollytunison@gmail.com>
2021-08-23 14:54:08 -0400

    updated DELETE movie

--- index.js
+++ index.js
@@ -14,61 +14,61 @@ app.use(bodyParser.json());
 
 // Array of objects with my top ten movies
 let topMovies = [{
-        Id: '1',
+        id: '1',
         title: 'The Godfather',
         director: 'Francis Ford Coppola',
         year: '1972'
     },
     {
-        Id: '2',
+        id: '2',
         title: 'Pulp Fiction',
         director: 'Quentin Tarantino',
         year: '1994'
     },
     {
-        Id: '3',
-        mtitle: 'The Shawshank Redemption',
+        id: '3',
+        title: 'The Shawshank Redemption',
         director: 'Frank Darabont',
         year: '1994'
     },
     {
-        Id: '4',
+        id: '4',
         title: 'The Good, the Bad and the Ugly',
         director: 'Sergio Leone',
         year: '1966'
     },
     {
-        Id: '5',
+        id: '5',
         title: 'Fight Club',
         director: 'David Fincher',
         year: '1999'
     },
     {
-        Id: '6',
+        id: '6',
         title: 'Gran Torino',
         director: 'Clint Eastwood',
         year: '2008'
     },
     {
-        Id: '7',
+        id: '7',
         title: 'Braveheart',
         director: 'Mel Gibson',
         year: '1995'
     },
     {
-        Id: '8',
+        id: '8',
         title: 'Star Wars',
         director: 'George Lucas',
         year: '1977'
     },
     {
-        Id: '9',
+        id: '9',
         title: 'Good Will Hunting',
         director: 'Gus Van Sant',
         year: '1997'
     },
     {
-        Id: '10',
+        id: '10',
         title: 'Apocalypse Now',
         director: 'Francis Ford Coppola',
         year: '1979'
@@ -111,10 +111,9 @@ app.delete('/movies/:id', (req, res) => {
     let movie = topMovies.find((movie) => {
         return movie.id === req.params.id
     });
-
     if (movie) {
         topMovies = topMovies.filter((obj) => { return obj.id !== req.params.id });
-        res.status(201).send('Movie with the ID of ' + req.params.id + ' was deleted.');
+        res.status(201).send('Movie with the id of ' + req.params.id + ' was deleted.');
     } else {
         res.status(404).send(`Movie with the id ${req.params.id} was not found.`);
     }
