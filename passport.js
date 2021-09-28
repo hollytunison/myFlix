83e52490aa917f8c918c2819e776d65b5fd9ec1c
Holly Tunison <hollytunison@gmail.com>
2021-09-28 15:51:39 -0400

    fixed passport.js period instead of comma

--- passport.js
+++ passport.js
@@ -1,6 +1,7 @@
 const passport = require('passport'),
-    LocalStrategy = require('passport-local').Strategy,
-    Models = require('./models.js'),
+    LocalStrategy = require('passport-local').
+Strategy,
+Models = require('./models.js'),
     passportJWT = require('passport-jwt');
 
 let Users = Models.User,
