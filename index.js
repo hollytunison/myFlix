2a0d9b8e65b1eb84c2cb406477e1afa54189cf01
Holly Tunison <hollytunison@gmail.com>
2021-09-28 10:55:08 -0400

    fixed add user

--- index.js
+++ index.js
@@ -106,7 +106,7 @@ app.get('/genres/:Name', passport.authenticate('jwt', { session: false }), (req,
 });
 
 //Add a user
-app.post('/users', passport.authenticate('jwt', { session: false }), (req, res) => {
+app.post('/users', (req, res) => {
     Users.findOne({ Username: req.body.Username })
         .then((user) => {
             if (user) {
