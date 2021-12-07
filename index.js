33e0c046787fca810a8978080de8226084c64780
Holly Tunison <hollytunison@gmail.com>
2021-12-07 09:28:18 -0700

    added back auth

--- index.js
+++ index.js
@@ -62,7 +62,7 @@ app.get('/', (req, res) => {
 });
 
 // GET a list of all movies
-app.get('/movies', (req, res) => {
+app.get('/movies', passport.authenticate('jwt', { session: false }), (req, res) => {
     Movies.find()
         .then(function(movies) {
             res.status(201).json(movies);
