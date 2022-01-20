13eb2df02588a629a45046ea3680443963b77a25
Holly Tunison <hollytunison@gmail.com>
2022-01-20 08:04:42 -0500

    fixed cors

--- index.js
+++ index.js
@@ -28,21 +28,16 @@ let allowedOrigins = [
     'https://my80svice.netlify.app/'
 ];
 
-app.use(
-	cors({
-		origin: (origin, callback) => {
-			if (!origin) return callback(null, true);
-			if (allowedOrigins.indexOf(origin) === -1) {
-				// If a specific origin isn’t found on the list of allowed origins
-				let message =
-					'The CORS policy for this application doesn’t allow access from origin ' +
-					origin;
-				return callback(new Error(message), false);
-			}
-			return callback(null, true);
-		},
-	})
-);
+app.use(cors({
+    origin: (origin, callback) => {
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.indexOf(origin) === -1) { // If a specific origin isn’t found on the list of allowed origins
+        let message = 'The CORS policy for this application doesn’t allow access from origin ' + origin;
+        return callback(new Error(message), false);
+      }
+      return callback(null, true);
+    }
+  }));
 
 // middleware
 app.use(bodyParser.json());
